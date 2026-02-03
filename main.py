ee3e2f11c2bc75861bd39ee92fc7364fa47aca8a
alexcleveland <alexcleveland5@gmail.com>
2026-02-02 18:55:47 -0700

    Enhancing with exception and error handling.

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import pandas as pd
 
 df = pd.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+program_complete = False
 
-name = input("What is your name?").upper()
-letters = [n for n in name]
-nato_pronunciation = [nato_dict[letter] for letter in letters]
-print(nato_pronunciation)
+while not program_complete :
+    try:
+        name = input("What is your name?").upper()
+        letters = [n for n in name]
+        nato_pronunciation = [nato_dict[letter] for letter in letters]
+        print(nato_pronunciation)
+    except KeyError:
+        print("Please only enter valid letters of the alphabet.")
+    else:
+        program_complete = True
